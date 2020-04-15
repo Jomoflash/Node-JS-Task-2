f716786c310af3586be54283b35a87c7a87b864b
Jomoflash <jomoflash22@gmail.com>
2020-04-15 16:19:32 +0100

    final commit for findInterns operation (c).

--- findInterns.js
+++ findInterns.js
@@ -1,16 +1,14 @@
 var MongoClient = require("mongodb").MongoClient;
 
-var url =  "mongodb://localhost:27017/";
+var url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function (err, db) {
     if (err) throw err;
 
     var dbo = db.db("Jomoflash")  //Switch to Jomoflash Database
     console.log(`Swithched to ${dbo.databaseName} database`);
 
-    var query = { rating : 7 };
-
-    dbo.collection("myMovies").find(query).toArray(function(err, result) {
+    dbo.collection("myMovies").find({}, { projection: { _id: 0, movie :1 } }).toArray(function (err, result) {
         if (err) throw err;
         console.log(result);
         db.close();
