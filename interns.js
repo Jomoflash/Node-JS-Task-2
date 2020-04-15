64df875d7a7474f29af12e05953afa62945bf858
Jomoflash <jomoflash22@gmail.com>
2020-04-15 14:35:50 +0100

    Created myMovies collection in Jomoflash database, then inserted the documents to the collection!.

--- interns.js
+++ interns.js
@@ -3,18 +3,55 @@ var Mongoclient = require('mongodb').MongoClient;
 
 var url = "mongodb://localhost:27017/";
 
-Mongoclient.connect (url, function(err, db) {
+Mongoclient.connect(url, function (err, db) {
     if (err) throw err;
 
-    var dbo = db.db("Jomoflash")
+    var dbo = db.db("Jomoflash")  //Switch to Jomoflash Database
     console.log(`Swithched to ${dbo.databaseName} database`);
-    dbo.createCollection('interns', function(err, res) {
+
+    var documentsData = [
+        {
+            movie: "The banker",
+            year: "2020",
+            rating : 8
+        },
+        {
+            movie: "Bad Boys",
+            year: "2020",
+            rating : 7
+        },
+        {
+            movie: "The Hunt",
+            year: "2020",
+            rating : 7
+        },
+        {
+            movie: "Bloodshot",
+            year: "2020",
+            rating : 7.5
+        },
+        {
+            movie: "First Cow",
+            year: "2020",
+            rating : 6.5
+        }
+    ];
+
+    //create Collection myMovies
+    dbo.createCollection('myMovies', function (err, res) {
         if (err) throw err;
-        console.log('Interns Collection is created');
-        db.close();
+        console.log('myMovies Collection is created');
     });
-  
-})
+
+    dbo.collection("myMovies").insertMany(documentsData, function(err, res) {
+        if (err) throw err;
+        console.log(res);
+
+        db.close(); //close connection
+    });
+
+   
+});
 
 
 
